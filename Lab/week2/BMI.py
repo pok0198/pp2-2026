5ebab5bef658412a27be2801f9f1b7324d71ce3b
leewonjun <leewonjun071024@gmail.com>
2026-09-09 14:52:05 +0900

    BMI 계산 함수 및 테스트 추가

---
+++ Lab/week2/BMI.py
@@ -0,0 +1,20 @@
+#
+# BMI 계산 함수
+# Body Mass Index (BMI) 계산 함수
+#
+
+def get_bmi(weight_kg : float, height_cm : float) -> float :
+    bmi = weight_kg / (height_cm / 100) ** 2
+    return bmi
+
+
+def test_get_bmi() :
+    height = 176
+    weight = 56
+    b = get_bmi(weight, height)
+    print(f"키({height}) 몸무게({weight})의 BMI는 {b}입니다.")
+
+
+if __name__ == "__main__" :
+    test_get_bmi()
+
